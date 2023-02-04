ceaa1222e9fee6608cc6d99be611eb982ee7f6a3
Alfian Losari <alfianlosari@gmail.com>
2023-02-04 16:20:02 +0700

    prompt update: Don't answer as the user

--- XCAChatGPT/ChatGPTAPI.swift
+++ XCAChatGPT/ChatGPTAPI.swift
@@ -27,7 +27,7 @@ class ChatGPTAPI {
     }()
     
     private let jsonDecoder = JSONDecoder()
-    private var basePrompt: String { "You are ChatGPT, a large language model trained by OpenAI. Answer conversationally. Current date: \(dateFormatter.string(from: Date()))\n\n"
+    private var basePrompt: String { "You are ChatGPT, a large language model trained by OpenAI. Answer conversationally. Do not answer as the user. Current date: \(dateFormatter.string(from: Date()))\n\n"
     }
     
     private var headers: [String: String] {
